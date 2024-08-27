dcb0806ed12f61ec3b835f08e30ce0447b5f5b13
Galych-Yuri <59996176+Galych-Yuri@users.noreply.github.com>
2024-08-27 16:00:43 +0300

    add file settings.py create class Settings init screen settings

---
+++ settings.py
@@ -0,0 +1,16 @@
+"""
+settings. Кожного разу коли ми додаватимемо новий функціонал
+простіше змінити певні налаштування ніж переписувати весь код
+"""
+
+
+class Settings:
+    """Клас для збереження всіх налаштувань гри."""
+
+    def __init__(self):
+        """initialize game settings"""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+
